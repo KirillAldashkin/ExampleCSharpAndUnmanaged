b32c2e717210c7ff411411c137e05fe2593dea4b
KirillAldashkin <a89086520092k@yandex.ru>
2021-11-21 20:53:40 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 0) Define external functions
+// 'unsafe' keyword is used to allow pointers
+[DllImport("libcpplib")]
+static extern unsafe void squareRoot(double* val);
+
+// 1) Create library resolver function
+// NOTE: this fuction may contain logic to return different libraries.
+// Parameters:
+//   name - Name of requested library (defined in 'DllImport' attribute)
+//   asm  - .NET assembly which requested library
+//   path - some options to search libraries
+IntPtr ResolveLib(string name, Assembly asm, DllImportSearchPath? path) => NativeLibrary.Load(name);
+
+// 2) Register library resolver function
+NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), ResolveLib);
+
+// 3) Use exteral functions
+unsafe
+{
+    double val = 25;
+    Console.WriteLine($"[C#] Value: {val}");
+    squareRoot(&val);
+    Console.WriteLine($"[C#] Value: {val}");
+    squareRoot(&val);
+    Console.WriteLine($"[C#] Value: {val}");
+}
---
+++ cpplib/Cpp.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void squareRoot(double* val)
+{
+	cout << "[C++] Value: " << val << endl;
+	*val = sqrt(*val);
+	cout << "[C++] Square root: " << val << endl;
+}
