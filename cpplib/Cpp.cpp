f3fdad4601c019ec093ba19ec0b42211e512d8a7
KirillAldashkin <a89086520092k@yandex.ru>
2021-11-21 21:30:45 +0800

    Fix

--- cpplib/Cpp.cpp
+++ cpplib/Cpp.cpp
@@ -1,11 +1,12 @@
-﻿#include <iostream>
+﻿#define DLL_EXPORT extern "C" __declspec(dllexport)
+#include <iostream>
 #include <cmath>
 
 using namespace std;
 
-void squareRoot(double* val)
+DLL_EXPORT void squareRoot(double* val)
 {
-	cout << "[C++] Value: " << val << endl;
+	cout << "[C++] Value: " << *val << endl;
 	*val = sqrt(*val);
-	cout << "[C++] Square root: " << val << endl;
+	cout << "[C++] Square root: " << *val << endl;
 }
