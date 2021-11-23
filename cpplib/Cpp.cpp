23371717bec1b6abb85689946b40953688623bcf
KirillAldashkin <a89086520092k@yandex.ru>
2021-11-23 19:39:59 +0800

    [Fix MacOS?] Explicit cast

--- cpplib/Cpp.cpp
+++ cpplib/Cpp.cpp
@@ -14,6 +14,7 @@ DLL_EXPORT void squareRoot(double* val)
 	*val = sqrt(*val);
 	cout << "[C++] Square root: " << *val << endl;
 }
+
 DLL_EXPORT double sumFunc(double from, double to, double step, double (*convFunc)(double))
 {
 	double ret = 0;
@@ -23,14 +24,15 @@ DLL_EXPORT double sumFunc(double from, double to, double step, double (*convFunc
 	}
 	return ret;
 }
+
 void fizzBuzz() { cout << " [C++] FizzBuzz" << endl; }
 void fizz() { cout << " [C++] Fizz" << endl; }
 void buzz() { cout << " [C++] Buzz" << endl; }
 void none() { cout << " [C++]" << endl; }
 DLL_EXPORT void* getFunction(int value)
 {
-	if (value % 15 == 0) return fizzBuzz;
-	else if (value % 5 == 0) return buzz;
-	else if (value % 3 == 0) return fizz;
-	else return none;
+	if (value % 15 == 0) return (void*)fizzBuzz;
+	else if (value % 5 == 0) return (void*)buzz;
+	else if (value % 3 == 0) return (void*)fizz;
+	else return (void*)none;
 }
